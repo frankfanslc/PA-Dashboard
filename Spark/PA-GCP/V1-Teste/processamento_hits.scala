d6b9862c12c6b35a42b0a46e49133cc109d6c8c2
monicaneli26 <nelimonica26@gmail.com>
2019-02-02 10:41:00 -0200

    Processando o número de pageviews_day

--- Spark/PA-GCP/V1-Teste/processamento_hits.scala
+++ Spark/PA-GCP/V1-Teste/processamento_hits.scala
@@ -14,9 +14,9 @@ import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat
 val conf = sc.hadoopConfiguration
 
 // Input parameters.
-val fullyQualifiedInputTableId = "publicdata:samples.shakespeare"
 val projectId = conf.get("fs.gs.project.id") //celtic-house-222820
 val bucket = conf.get("fs.gs.system.bucket")
+val fullyQualifiedInputTableId = projectId + ":GA_raw.hits_data"
 
 // Input configuration.
 conf.set(BigQueryConfiguration.PROJECT_ID_KEY, projectId)
@@ -25,9 +25,10 @@ BigQueryConfiguration.configureBigQueryInput(conf, fullyQualifiedInputTableId)
 
 
 // Output parameters.
-val outputTableId = projectId + ":GA_raw.wordcount_spark_output"
+val outputTableId = projectId + ":GA_raw.hits_processed_output"
+
 // Temp output bucket that is deleted upon completion of job.
-val outputGcsPath = ("gs://" + bucket + "/hadoop/tmp/bigquery/wordcountoutput")
+val outputGcsPath = ("gs://" + bucket + "/hadoop/tmp/bigquery/hitsoutput")
 
 // Output configuration.
 // Let BigQuery auto-detect output schema (set to null below).
@@ -47,14 +48,38 @@ conf.set("mapreduce.job.outputformat.class", classOf[IndirectBigQueryOutputForma
 // Truncate the table before writing output to allow multiple runs.
 conf.set(BigQueryConfiguration.OUTPUT_TABLE_WRITE_DISPOSITION_KEY, "WRITE_TRUNCATE")
 
-// Helper to convert JsonObjects to (word, count) tuples.
+/*Helper to convert JsonObjects to (word, count) tuples.
 def convertToTuple(record: JsonObject) : (String, Long) = {
   val word = record.get("word").getAsString.toLowerCase
   val count = record.get("word_count").getAsLong
   return (word, count)
+}*/
+
+// Pageview, contar as linhas de hits do dia
+def convertToTuple(record: JsonObject) : (String, Long) = {
+  val clientId = record.get("clientId").getAsString
+  val count = 0
+  return (clientId, count)
 }
 
-// Helper to convert (word, count) tuples to JsonObjects.
+// Pageview, contar as linhas de hits do dia
+def convertToTupleInitialized(record: JsonObject, numTotalHits : Long) : (String, Long) = {
+  val clientId = record.get("clientId").getAsString
+  val count = numTotalHits
+  return (clientId, count)
+}
+
+// Helper to convert (clientId, count) tuples to JsonObjects
+def convertToJson(pair: (String, Long)) : JsonObject = {
+  val clientId = pair._1
+  val count = pair._2
+  val jsonObject = new JsonObject()
+  jsonObject.addProperty("clientId", clientId)
+  jsonObject.addProperty("pageviews_day", count)
+  return jsonObject
+}
+
+/* Helper to convert (word, count) tuples to JsonObjects.
 def convertToJson(pair: (String, Long)) : JsonObject = {
   val word = pair._1
   val count = pair._2
@@ -62,7 +87,7 @@ def convertToJson(pair: (String, Long)) : JsonObject = {
   jsonObject.addProperty("word", word)
   jsonObject.addProperty("word_count", count)
   return jsonObject
-}
+}*/
 
 // Load data from BigQuery.
 val tableData = sc.newAPIHadoopRDD(
@@ -72,15 +97,38 @@ val tableData = sc.newAPIHadoopRDD(
     classOf[JsonObject])
 
 // Perform word count.
-val wordCounts = (tableData
+/*val wordCounts = (tableData
     .map(entry => convertToTuple(entry._2))
     .reduceByKey(_ + _))
+*/
+//Contagem de linhas de hits (Pageviews_day)
+val numLinhasRDD = tableData.count()
+
+// Display results pageView_day
+println(numLinhasRDD)
+//val wordCountsTeste = (tableData.map(entry => convertToTuple(entry._2)))    
+//wordCountsTeste.take(10).foreach(l => println(l))
+
+var hitsProcessed = (tableData.map(entry => convertToTupleInitialized(entry._2, numLinhasRDD)))
 
 // Display 10 results.
-wordCounts.take(10).foreach(l => println(l))
+hitsProcessed.take(10).foreach(l => println(l))
+
+// Display 10 results.
+//wordCounts.take(10).foreach(l => println(l))
+
+
 
 // Write data back into a new BigQuery table.
 // IndirectBigQueryOutputFormat discards keys, so set key to null.
-(wordCounts
+/*(wordCounts
     .map(pair => (null, convertToJson(pair)))
     .saveAsNewAPIHadoopDataset(conf))
+*/
+
+// Write data back into a new BigQuery table.
+// IndirectBigQueryOutputFormat discards keys, so set key to null.
+(hitsProcessed
+    .map(pair => (null, convertToJson(pair)))
+    .saveAsNewAPIHadoopDataset(conf))
+
